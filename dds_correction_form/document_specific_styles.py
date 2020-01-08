7b7d2835164ce66a19bcc7561830b54a6f9a7a8e
tbyronb <tom@tombyron.digital>
2020-01-08 15:06:54 -0800

    dds correction form template

--- dds_correction_form/document_specific_styles.py
+++ dds_correction_form/document_specific_styles.py
@@ -1,3 +1,71 @@
-from common.reportlab_styles import *
+from common.reportlab_styles import extend_style, extend_table_style
+import os
+from reportlab.lib.enums import TA_LEFT
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import mm
+from reportlab.pdfbase.pdfmetrics import registerFont, registerFontFamily
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import TableStyle
 
-# document specific styles go here
+registerFont(
+    TTFont("Arial", os.path.join(os.getcwd(), "fonts", "arial.ttf"))
+)
+registerFont(
+    TTFont("Arial-Bold", os.path.join(os.getcwd(), "fonts", "arialbd.ttf"))
+)
+registerFont(
+    TTFont("Arial-Italic", os.path.join(os.getcwd(), "fonts", "ariali.ttf"))
+)
+registerFont(
+    TTFont("Arial-BoldItalic", os.path.join(os.getcwd(), "fonts", "arialbi.ttf"))
+)
+registerFontFamily(
+    "Arial",
+    normal="Arial",
+    bold="Arial-Bold",
+    italic="Arial-Italic",
+    boldItalic="Arial-BoldItalic",
+)
+
+styles = dict()
+styles["main"] = ParagraphStyle(
+    "main",
+    fontSize=12,
+    leading=14,
+    trailing=0,
+    spaceBefore=0,
+    spaceAfter=0,
+    leftIndent=0,
+    rightIndent=0,
+    wordWrap=None,
+    alignment=TA_LEFT,
+    fontName="Arial",
+)
+styles["doc-header"] = ParagraphStyle(
+    "doc-header",
+    parent=styles["main"],
+    fontSize=16,
+    leading=18.5,
+    textColor="white",
+)
+styles["field-label"] = ParagraphStyle(
+    "field-label",
+    parent=styles["main"],
+)
+styles["field-value"] = ParagraphStyle(
+    "field-value",
+    parent=styles["main"],
+)
+styles["main-table"] = TableStyle([
+    ("LEFTPADDING", (0, 0), (-1, -1), 0),
+    ("RIGHTPADDING", (0, 0), (-1, -1), 0),
+    ("TOPPADDING", (0, 0), (-1, -1), 0),
+    ("BOTTOMPADDING", (0, 0), (-1, -1), 0),
+])
+styles["section-table"] = TableStyle([
+    ("OUTLINE", (0, 0), (-1, -1), 0.8 * mm, "black", "projecting"),
+    ("LEFTPADDING", (0, 0), (-1, -1), 3 * mm),
+    ("RIGHTPADDING", (0, 0), (-1, -1), 3 * mm),
+    ("TOPPADDING", (0, 0), (-1, -1), 1.4 * mm),
+    ("BOTTOMPADDING", (0, 0), (-1, -1), 4 * mm),
+])
