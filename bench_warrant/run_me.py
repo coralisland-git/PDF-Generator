915e014314b6905e14647fd048b21330d893d9e3
coralisland-git <coralisland8327@gmail.com>
2020-01-15 19:20:53 +0000

    path

--- bench_warrant/run_me.py
+++ bench_warrant/run_me.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     from sample_data import bench_warrant_data
 
     doc_result = generate_bench_warrant(bench_warrant_data)
-    with open(os.path.expanduser("~/bench_warrant.pdf"), "wb+") as output_file:
+    with open(os.path.expanduser("~/Desktop/bench_warrant.pdf"), "wb+") as output_file:
         shutil.copyfileobj(doc_result["document"], output_file)
     with open(os.path.expanduser("~/Desktop/bench_warrant.json"), "w+") as output_file:
         output_file.write(json.dumps(doc_result["metadata"], indent=4))
