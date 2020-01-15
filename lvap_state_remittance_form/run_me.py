5ce97fff38b9aa9d2925c8c7e6e367ff73efe087
coralisland-git <coralisland8327@gmail.com>
2020-01-15 19:14:26 +0000

    path

--- lvap_state_remittance_form/run_me.py
+++ lvap_state_remittance_form/run_me.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     from sample_data import lvap_state_remittance_form_data
 
     with open(
-        os.path.expanduser("lvap_state_remittance_form.pdf"), "wb+"
+        os.path.expanduser("~/Desktop/lvap_state_remittance_form.pdf"), "wb+"
     ) as output_file:
         shutil.copyfileobj(
             generate_lvap_state_remittance_form(lvap_state_remittance_form_data),
