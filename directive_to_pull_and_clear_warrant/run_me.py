d8ed979464b8369c002d0cef0290ac6f67e6a8c6
Mazzen <msaab@quicketsolutions.com>
2020-02-05 15:41:58 -0600

    project_setup-mazzen

---
+++ directive_to_pull_and_clear_warrant/run_me.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+from generate_directive_to_pull_and_clear_warrant import (
+    generate_directive_to_pull_and_clear_warrant,
+)
+
+if __name__ == "__main__":
+    buff = generate_directive_to_pull_and_clear_warrant()
+
+    with open(
+        os.path.expanduser("~/Desktop/directive_to_pull_and_clear_warrant.pdf"), "wb+"
+    ) as output_file:
+        shutil.copyfileobj(buff, output_file)
+
+    print("completed")
