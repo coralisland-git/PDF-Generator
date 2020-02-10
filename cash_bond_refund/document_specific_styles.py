a6879b9102739dd5f34b0ea65e258a8c4651670a
coralisland-git <coralisland8327@gmail.com>
2020-02-10 21:23:35 +0000

    add generate_cash_bond_refund

--- cash_bond_refund/document_specific_styles.py
+++ cash_bond_refund/document_specific_styles.py
@@ -1,3 +1,19 @@
 from common.reportlab_styles import *
 
 # document specific styles go here
+
+styles['rc-doc-header-fda'] = ParagraphStyle(
+	"rc-doc-header-fda",
+	parent=styles["rc-doc-header"],
+	fontName='Arial-Bold',
+	fontSize=14
+)
+
+styles["rc-aawp-main-content"] = ParagraphStyle(
+    "rc-aawp-main-content",
+    parent=styles["rc-aawp-main"],
+    fontName="Arial",
+    fontSize=14,
+    leading=15,
+    spaceBefore=14
+)
--- cash_bond_refund/generate_cash_bond_refund.py
+++ cash_bond_refund/generate_cash_bond_refund.py
@@ -5,16 +5,107 @@ from common.signatures import *
 
 
 def generate_cash_bond_refund():
+    cr = CBRReport()
     buff = cStringIO.StringIO()
-    doc = BaseDocTemplate(buff, pagesize=letter)
-    f = Frame(gutters[0], gutters[2], usable_width, usable_height, showBoundary=0)
+    return cr.create_report(buff)
 
-    story = []
 
-    main_template = PageTemplate(id="main_template", frames=[f])
-    doc.addPageTemplates([main_template])
-    doc.build(story, canvasmaker=PageNumCanvas)
-    del doc
+class CBRReport:
+    def __init__(self, title=None, author=None):
+        self.page_size = letter
+        self.page_margin = (24.4 * mm, 14.4 * mm)
+        self.sections = ["content"]
+        self.title = title
+        self.author = author
+        self.data = None
 
-    buff.seek(0)
-    return buff
+    def create_report(self, buff=None):
+        def get_method(section):
+            try:
+                method = getattr(self, "_section_" + section)
+            except AttributeError:
+                raise Exception("Section method not found: " + section)
+            return method     
+
+        if not buff:
+            buff = io.BytesIO()
+
+        story = []
+        for section in self.sections:
+            elems = get_method(section)()
+            for elem in elems:
+                story.append(elem)
+
+        page_t = PageTemplate('normal', [
+            Frame(
+                self.page_margin[0],
+                self.page_margin[1],
+                self.page_size[0] - self.page_margin[0] * 2,
+                self.page_size[1] - self.page_margin[1] * 2,
+                leftPadding=0,
+                bottomPadding=0,
+                rightPadding=0,
+                topPadding=0,
+            )
+        ])
+        doc_t = BaseDocTemplate(
+            buff,
+            pagesize=letter,
+            title=self.title,
+            author=self.author,
+            leftMargin=self.page_margin[0],
+            rightMargin=self.page_margin[0],
+            topMargin=self.page_margin[1],
+            bottomMargin=self.page_margin[1],
+        )
+        doc_t.addPageTemplates(page_t)
+        doc_t.build(story)
+        buff.seek(0)
+        return buff
+
+    def _section_content(self):
+        elems = list()
+        elems += [
+            Spacer(0, 12.4 * mm),
+            Image('brookhaven.jpg', 72 * mm, 31.5 * mm),
+            Spacer(0, 32 * mm),
+            Paragraph(
+                "CASH BOND REFUND",
+                styles["rc-doc-header-fda"]
+            ),
+            Spacer(0, 9.8 * mm),
+            Paragraph(
+                "DATE:"+"&nbsp;"*8+"<u>March 14, 2018</u>", 
+                styles["rc-aawp-main-content"]
+            ),
+            Paragraph(
+                "Defendant's Name:"+"&nbsp;"*5+"<u>Andres Ayona</u>", 
+                styles["rc-aawp-main-content"]
+            ),
+            Paragraph(
+                "Citation#:"+"&nbsp;"*2+"<u>E39566</u>",
+                extend_style(styles["rc-aawp-main-content"])
+            ),
+            Paragraph(
+                "CHECK #:"+"&nbsp;"*2+"<u>22636</u>",
+                extend_style(styles["rc-aawp-main-content"], spaceBefore=42)
+            ),
+            Paragraph(
+                "AMOUNT:"+"&nbsp;"+"$ <u>525.00</u>", 
+                extend_style(styles["rc-aawp-main-content"], spaceBefore=42)
+            ),
+            Paragraph(
+                "RECEIVED BY: "+"_"*28, 
+                extend_style(styles["rc-aawp-main-content"], spaceBefore=42)
+            ),
+            Paragraph(
+                "Date: "+"_"*20, 
+                styles["rc-aawp-main-content"]
+            ),
+            Paragraph(
+                "<u>Merced Mendez</u>", 
+                styles["rc-aawp-main-content"]
+            )
+        ]
+        
+        return elems
