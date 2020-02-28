f29d15a82d51ff2e3466c95fcaf899713105f042
coralisland-git <coralisland8327@gmail.com>
2020-02-28 14:21:19 +0000

    add generate_entry_of_appearance

--- entry_of_appearance/document_specific_styles.py
+++ entry_of_appearance/document_specific_styles.py
@@ -1,3 +1,19 @@
 from common.reportlab_styles import *
 
 # document specific styles go here
+
+styles['rc-doc-header'] = ParagraphStyle(
+	"rc-doc-header",
+	parent=styles["rc-doc-header"],
+    fontName="Times-Roman",
+	spaceBefore=24,
+	fontSize=10.5,
+	leading=12
+)
+
+styles["rc-aawp-main-content"] = ParagraphStyle(
+    "rc-aawp-main-content",
+    parent=styles["rc-aawp-main"],
+    fontSize=11,
+    leading=14.5
+)
