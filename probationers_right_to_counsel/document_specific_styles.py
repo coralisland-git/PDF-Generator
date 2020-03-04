bdb550ea76681597e644fa27bbf9633a1408ef2c
coralisland-git <coralisland8327@gmail.com>
2020-03-04 20:19:13 +0000

    add generate_probationers_right_to_counsel

--- probationers_right_to_counsel/document_specific_styles.py
+++ probationers_right_to_counsel/document_specific_styles.py
@@ -1,3 +1,18 @@
 from common.reportlab_styles import *
 
 # document specific styles go here
+
+styles["rc-tdwp-main"] = ParagraphStyle(
+    "rc-tdwp-main",
+    parent=styles["rc-main"],
+    fontSize=12,
+    spaceBefore=14
+)
+
+styles["rc-tdwp-main-tb"] = ParagraphStyle(
+    "rc-tdwp-main-tb",
+    parent=styles["rc-tdwp-main"],
+    fontSize=12,
+    leading=13,
+    spaceBefore=0
+)
--- probationers_right_to_counsel/run_me.py
+++ probationers_right_to_counsel/run_me.py
@@ -4,11 +4,16 @@ import shutil
 from generate_probationers_right_to_counsel import generate_probationers_right_to_counsel
 
 if __name__ == "__main__":
-    buff = generate_probationers_right_to_counsel()
+    buff_en, buff_sp = generate_probationers_right_to_counsel()
 
     with open(
-        os.path.expanduser("~/Desktop/{}.pdf".format("probationers_right_to_counsel")), "wb+"
+        os.path.expanduser("{}.pdf".format("probationers_right_to_counsel_english")), "wb+"
     ) as output_file:
-        shutil.copyfileobj(buff, output_file)
+        shutil.copyfileobj(buff_en, output_file)
+
+    with open(
+        os.path.expanduser("{}.pdf".format("probationers_right_to_counsel_spanish")), "wb+"
+    ) as output_file:
+        shutil.copyfileobj(buff_sp, output_file)
 
     print("completed")
