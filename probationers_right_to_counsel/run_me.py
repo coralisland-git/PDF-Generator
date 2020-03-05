b3a1c9584f1d1f3ec0e8dc25fc35233c83e6ab14
Mazzen <msaab@quicketsolutions.com>
2020-03-05 11:44:11 -0600

    PRTC: Destination change

--- probationers_right_to_counsel/run_me.py
+++ probationers_right_to_counsel/run_me.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     buff_en, buff_sp = generate_probationers_right_to_counsel()
 
     with open(
-        os.path.expanduser("{}.pdf".format("probationers_right_to_counsel_english")), "wb+"
+        os.path.expanduser("~/Desktop/{}.pdf".format("probationers_right_to_counsel_english")), "wb+"
     ) as output_file:
         shutil.copyfileobj(buff_en, output_file)
 
     with open(
-        os.path.expanduser("{}.pdf".format("probationers_right_to_counsel_spanish")), "wb+"
+        os.path.expanduser("~/Desktop/{}.pdf".format("probationers_right_to_counsel_spanish")), "wb+"
     ) as output_file:
         shutil.copyfileobj(buff_sp, output_file)
 
