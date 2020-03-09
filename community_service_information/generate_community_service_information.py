c25eceeb4f9d6da9209fa716c8c3bbf2caa8018b
Mazzen <msaab@quicketsolutions.com>
2020-03-09 15:20:50 -0500

    CSI: Typos

--- community_service_information/generate_community_service_information.py
+++ community_service_information/generate_community_service_information.py
@@ -90,7 +90,7 @@ class CSIReport:
                                         34 Peachtree Street <br />
                                         Suite 1000 <br />
                                         Atlanta, GA 30303 <br />
-                                        (404) 591-3180    Fax (404)478-9515
+                                        (404) 591-3180    Fax (404) 478-9515
                                         """, 
                                         styles["rc-tdwp-main-header-box"]
                                     )
@@ -113,7 +113,7 @@ class CSIReport:
                 colWidths=(60*mm, 82*mm, 51*mm)
             ),
             Paragraph(
-                "COMMUNTITY SERVICE INFORMATION",                
+                "COMMUNITY SERVICE INFORMATION",
                 styles["rc-tdwp-main-header"]
             ),            
             Paragraph(
@@ -133,11 +133,11 @@ class CSIReport:
             Table(
                 [
                     [
-                        Paragraph("1. American Canner Society", styles["rc-tdwp-main-tb"]),
-                        Paragraph("6. Parks-city, county", styles["rc-tdwp-main-tb"])
+                        Paragraph("1. American Cancer Society", styles["rc-tdwp-main-tb"]),
+                        Paragraph("6. Parks - city, county", styles["rc-tdwp-main-tb"])
                     ],
                     [
-                        Paragraph("2. Disable Veteran's Center", styles["rc-tdwp-main-tb"]),
+                        Paragraph("2. Disabled Veteran's Center", styles["rc-tdwp-main-tb"]),
                         Paragraph("7. Public Library", styles["rc-tdwp-main-tb"])
                     ],
                     [
@@ -188,7 +188,7 @@ class CSIReport:
                     ],
                     [
                         Paragraph("2.", styles["rc-tdwp-main-tb"]),
-                        Paragraph("Grounds maintenance, litter pick-up, yard work; gardening, landscape, making highway signs, street and highway maintenance.", styles["rc-tdwp-main-tb"])
+                        Paragraph("Grounds maintenance, litter pick-up, yard work, gardening, landscape, making highway signs, street and highway maintenance.", styles["rc-tdwp-main-tb"])
                     ],
                     [
                         Paragraph("3.", styles["rc-tdwp-main-tb"]),
@@ -200,7 +200,7 @@ class CSIReport:
                     ],
                     [
                         Paragraph("5.", styles["rc-tdwp-main-tb"]),
-                        Paragraph("Human services, patient services, visiting senior citizens, assisting disabled veterans and seniors, teacher's aide", styles["rc-tdwp-main-tb"])                        
+                        Paragraph("Human services, patient services, visiting senior citizens, assisting disabled veterans and seniors, teacher's aide.", styles["rc-tdwp-main-tb"])
                     ]
                 ],
                 style=extend_table_style(styles["rc-main-table"], [
@@ -242,7 +242,7 @@ class CSIReport:
                                     Paragraph("404 872 8089", styles["rc-tdwp-main-tb"])
                                 ],
                                 [
-                                    Paragraph("Atlanta Humans Society", styles["rc-tdwp-main-tb"]),
+                                    Paragraph("Atlanta Humane Society", styles["rc-tdwp-main-tb"]),
                                     Paragraph("404 974-2842", styles["rc-tdwp-main-tb"])
                                 ],
                                 [
@@ -254,7 +254,7 @@ class CSIReport:
                                     Paragraph(
                                         """
                                         Brady Center 404 669-3776 <br />
-                                        Conley Center 404 669-3993 <br />
+                                        Conley Center 404 669-3773 <br />
                                         Godby Road 404 669-9206 <br />
                                         """, styles["rc-tdwp-main-tb"])
                                 ],
@@ -267,11 +267,11 @@ class CSIReport:
                                     Paragraph("404 730-1965", styles["rc-tdwp-main-tb"])
                                 ],
                                 [
-                                    Paragraph("Mark Wade Fulton Country Jail", styles["rc-tdwp-main-tb"]),
+                                    Paragraph("Mark Wade Fulton County Jail", styles["rc-tdwp-main-tb"]),
                                     Paragraph("404 472-5639", styles["rc-tdwp-main-tb"])
                                 ],
                                 [
-                                    Paragraph("Daily Bread for A.ll Credo Credolawson 8 to 6 pm 770905-2866", styles["rc-tdwp-main-tb"]),
+                                    Paragraph("Daily Bread for All Credo Credolawson 8 to 6 pm 770 905-2866", styles["rc-tdwp-main-tb"]),
                                     Paragraph("1234 Beaver Ruin Rd Suite 204 Norcross; 385 Born St. Ste D Lawrenceville; 5891 New P'tree Rd. Dorvaille", styles["rc-tdwp-main-tb"])
                                 ]
                             ],
@@ -297,72 +297,73 @@ class CSIReport:
         elems = [
             Paragraph(
                 """
-                AMERICAN CANCER SOCIETY - 770/419-DOE3  <br />
+                AMERICAN CANCER SOCIETY - 770/429-0089  <br />
                 AMERICAN HEART ASSOCIATION - 678/224-2000  <br />
-                AMERICAN HUMAN SOCIETY 404 974-2847 01:32 WEDNESDAY 9:30 <br />
-                AMERCIAN LEGIN-AWARDALE EST 404 292-288 <br />
+                AMERICAN HUMANE SOCIETY 404/974-2847 01:32 WEDNESDAY 9:30 <br />
+                AMERICAN LEGION - AVONDALE EST 404/292-2352 <br />
                 AMERICAN RED CROSS - 770/852-4318 <br />
-                ANGEL FLIGHT- DEKAL PEACHTREE AIRPORT- 770/452-7958 <br />
+                ANGEL FLIGHT- DEKALB PEACHTREE AIRPORT - 770/452-7958 <br />
                 ANIMAL SHELTERS / ANIMAL RESCUE ORGANIZATIONS  <br />
                 ATLANTA BOTANICAL GARDENS - 404/591-1548 <br />
                 ATLANTA COMMUNITY FOOD BANK - 404/892-3333 <br />
+                ATLANTA HUMANE SOCIETY - 404/974-2842 or 22 Wednesdays at 9:30 <br />
                 ATLANTA MISSION - ATLANTA - 404/588-4000 <br />
-                ATLANTA MISSION/MARIETTA THRIFT STORE - MARIETTA - 404/357-3920 <br />
+                ATLANTA MISSION/MARIETTA THRIFT STORE - MARIETTA - 404/367-3920 <br />
                 ATLANTA WILD ANIMAL RESCUE - LITHONIA - (<u>www.awareone.org</u>) 678/418-1111 <br />
                 AUTREY MILL NATURE PRESERVE & HERITAGE CENTER - JOHN'S CREEK - 678/366-3511 <br /> 
-                BE SOMEONE, INC. - STONE MOUNTAIN -770/465-644S <br />
+                BE SOMEONE, INC. - STONE MOUNTAIN - 770/465-644S <br />
                 BLESSED ANGELS, INC - AUSTELL - 404/323-1156 - (CHRISTINE WASHINGTON) <br />
                 BOY SCOUTS OF AMERICA - ATLANTA - 770/956-3176 <br />
                 BOYS & GIRLS CLUB OF ATLANTA - 404/527-7115 <br />
-                BUCKHEAD CHRISTIAN MINISTRY 404/239-0058 x 111 <br />
-                CHASTIAN PARK CONSERVANCY - 404/237-2177 <br />
-                CHEROKEE COUNTY HUMANE SOCIETY THRIFT STORE - ACWOFCTH - 770/592-8072  <br />
-                CHILDREN'S RESTORATION NETWORK - ROSWELL - 770/393-7117 <br />
+                BUCKHEAD CHRISTIAN MINISTRY - 404/239-0058 x 111 <br />
+                CHASTAIN PARK CONSERVANCY - 404/237-2177 <br />
+                CHEROKEE COUNTY HUMANE SOCIETY THRIFT STORE - ACWORTH - 770/592-8072  <br />
+                CHILDREN'S RESTORATION NETWORK - ROSWELL - 770/649-7117 <br />
                 CHILDREN'S WISH FOUNDATION - ATLANTA - 770/393-0683 <br /><br />
-                COBS CHRISTMAS - MARIETTA - 678/439-9627 <br />
+                COBB CHRISTMAS - MARIETTA - 678/439-9627 <br />
                 COMMUNITY FELLOWSHIP HOMELESS MINISTRY - 404/428-8243  <br />
                 DAUSET TRAILS NATURE CENTER - BUTTS COUNTY - 770/775-6798  <br />
-                DIABETES ASSOC - ATLANTA 404/527-7150 x 119 <br />
+                DIABETES ASSOC - ATLANTA - 404/527-7150 x 119 <br />
                 ELACHEE NATURE SCIENCE CENTER - GAINESVILLE - 770/535-1976 <br />
-                FAIR OAKS UNITED METHODIST CHURCH - MARIETTA - 770/428-2658 (ENGLISH/SPANISH/BRAZILLAN)  <br />
+                FAIR OAKS UNITED METHODIST CHURCH - MARIETTA - 770/428-2658 (ENGLISH/SPANISH/BRAZILIAN)  <br />
                 FAITH EQUESTRIAN THERAPEUTIC CENTER - EFFINGHAM COUNTY - 912/728-3728 <br />
                 FEED MY LAMBS - MARIETTA - 770/795-9349  <br />
                 FEEDING GOD'S CHILDREN MINISTRY - MARIETTA - 404/384-5095 (PASTOR BYNUM)  <br />
                 FORGOTTEN PAWS RESCUE (<u>www.forgottenpaws.com</u>) <br />
                 FORT YARGO STATE PARK - WINDER - 770/867-3489 <br />
-                FOUNDATION FOR HOSPITAL ART - ATLANTA - (<u>www.hospitalart.com</u>) 678/324-1705  <br />
+                FOUNDATION FOR HOSPITAL ART - ATLANTA - (<u>www.hospitalart.com</u>) - 678/324-1705  <br />
                 GIRL SCOUTS OF GREATER ATLANTA - 770/702-9100  <br />
                 HABITAT FOR HUMANITY METRO ATLANTA - 770/432-7954 <br />
-                HAND MAIDANS MMISTRIES FOOD PAHTN 770 990-2373
+                HANDMAIDENS MINISTRIES FOOD PANTRY - 770/990-2373
                 HANDS ON ATLANTA - 404/979-2800 <br />
                 HELPING IN HIS NAME MINISTRIES FOOD PANTRY - HENRY COUNTY - 678/565-6135  <br />
-                HAVEN HILLS THERAPEUTIC RIDING CENTER - FAIRBURN - 578/296-9693 <br />
+                HAVEN HILLS THERAPEUTIC RIDING CENTER - FAIRBURN - 678/296-9693 <br />
                 HOMELESS PETS FOUNDATION - MARIETTA - 770/971-0100 <br />
                 H.O.P.E. THROUGH DIVINE INTERVENTION - ATLANTA - 404/748-4375 <br />
                 HOSEA FEED THE HUNGRY - ATLANTA - 404/755-3353 x 315 <br />
                 IN THE MEAN TIME MINISTRIES (SHEPHERD'S HOUSE) - MARIETTA - 770/792-0097 (<u>www.inthemeantime.org</u>) <br />
                 JERUSALEM HOUSE - ATLANTA - 404/377-3443 <br />
-                KEEP SMYRNA BEAUTIFUL(RECYCLING CENTER) - SIVIYRINLA - 770/431-2863 <br />
-                KENNESAW MOUNTAIN NATIONAL PARK - 770/427-4685 <br />
+                KEEP SMYRNA BEAUTIFUL (RECYCLING CENTER) - SMYRNA - 770/431-2863 <br />
+                KENNESAW MOUNTAIN NATIONAL PARK - 770/427-4686 <br />
                 LIFELINE ANIMAL PROJECT - ATLANTA - (<u>www.info@atlantapets.org</u>) - 404/292-8800 <br />
                 LOAVES & FISHES @ ST.JOHN'S ORTHODOX CHURCH - ATLANTA - 404/577-6330  <br />
-                MCFCENNA FARMS - DALLAS - 770/433-9672 (<u>www.mckennafarmstherapy.org</u>) <br />
-                MAKE A WAY HOUSING - ATLANTA - 404/792-3011 <br />
+                MCKENNA FARMS - DALLAS - 770/433-9672 (<u>www.mckennafarmstherapy.org</u>) <br />
+                MAKE A WAY HOUSING - ATLANTA - 404/792-8011 <br />
                 MARCH OF DIMES - 404/350-9800 <br />
                 MOSTLY MUTTS - KENNESAW - 770/325-7387 <br />
                 MUST MINISTRIES - 770/427-9862 <br />
                 NEVER ALONE MINISTRIES - WOODSTOCK - 770/363-5272 (<u>www.neveralone.org</u>) <br />
                 NOAH'S ARK - LOCUST GROVE - 770/957-0888 <br />
-                NORTH FULTON COMMUNITY CHARITIES - RDSWELL 770/64D-0399  <br />
-                OUR FATHER'S HANDS - POWDER SPRINGS - 770/222/6775 <br />
+                NORTH FULTON COMMUNITY CHARITIES - ROSWELL - 770/640-0399  <br />
+                OUR FATHER'S HANDS - POWDER SPRINGS - 770/222-6775 <br />
                 OUR PAL'S PLACE - MARIETTA - 678/795-0202 <br />
-                P.A.L.S (PETS ARE LOVING SUPPORT) - ATLANTA - 404/875-PALS  <br />
+                P.A.L.S (PETS ARE LOVING SUPPORT) - ATLANTA - 404/876-PALS  <br />
                 PAWS ATLANTA - DECATUR - 770/593-115S <br />
-                PICK OF THE LITTER - HIRAM -678/977-4925 (<u>www.ourpickofthelitter.com</u>)  <br />
-                PREDMONT PANK CANSENANCY 404 875-7275 ext 247 <br />
+                PICK OF THE LITTER - HIRAM - 678/977-4925 (<u>www.ourpickofthelitter.com</u>)  <br />
+                PREDMONT PARK CONSERVANCY 404 875-7275 ext 247 <br />
                 RIGHT IN THE COMMUNITY - MARIETTA - 770/427-8401 <br />
-                ROSWELL RECYCLING CENTER 770 442-8822 <br />
-                SAFERIDE - 404 888-0887 <br />
+                ROSWELL RECYCLING CENTER 770/442-8822 <br />
+                SAFERIDE - 404/888-0887 <br />
                 SCATTERING RESOURCES - SMYRNA - 678/729-7228 <br /> 
                 """,
                 extend_style(styles["rc-tdwp-main-list"])
