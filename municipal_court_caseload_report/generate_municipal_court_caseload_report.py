82d6ea6a47e7b76d60d235aa1a40e2421089c7d1
coralisland-git <coralisland8327@gmail.com>
2020-03-23 18:10:25 +0000

    correction

--- municipal_court_caseload_report/generate_municipal_court_caseload_report.py
+++ municipal_court_caseload_report/generate_municipal_court_caseload_report.py
@@ -98,17 +98,19 @@ class MCCRReport:
                                 ],
                                 [
                                     Paragraph(
-                                        "OBTAIN FINGER PRINTS", 
+                                        """
+                                        BROOKHAVEN MUNICIPAL COURT <br />
+                                        Municipal Court Caseload Report
+                                        """, 
                                         styles["rc-doc-header"]
                                     )
                                 ],
                                 [
                                     Paragraph(
                                         """
-                                        STATE OF GEORGIA <br />
-                                        DEKALB COUNTY
+                                        CASE COUNTS FOR: 06/01/2019 - 06/30/2019
                                         """,
-                                        extend_style(styles["rc-doc-header"], fontSize=12)
+                                        extend_style(styles["rc-aawp-main-content"], fontSize=12, alignment=TA_CENTER)
                                     )
                                 ]
                             ],
@@ -131,177 +133,86 @@ class MCCRReport:
                 ]),
                 colWidths=(42*mm, 112*mm, 43*mm)
             ),
-            Spacer(0, 6.4*mm),
-            Table(
-                [   
-                    [
-                        Table(
-                            [   
-                                [
-                                    Paragraph("<b>REPORT TO:</b> ", extend_style(styles['rc-aawp-main-content'])),
-                                    Paragraph(
-                                        """
-                                        BROOKHAVEN POLICE DEPARTMENT <br />
-                                        2665 BUFORD HWY <br />
-                                        BROOKHAVEN, GA 30324 <br />
-                                        404-637-0600
-                                        """,
-                                        extend_style(styles['rc-aawp-main-content'])
-                                    )
-                                ]
-                            ],
-                            style=extend_table_style(styles["rc-main-table"], [
-                                ("VALIGN", (0, 0), (-1, -1), "TOP")
-                            ]),
-                            colWidths=(28*mm, 77*mm)
-                        ),
-                        Table(
-                            [   
-                                [
-                                    Paragraph("<b>HOURS:</b>", styles['rc-aawp-main-content']),
-                                    Paragraph(
-                                        """
-                                        TUESDAY & THURSDAY  <br />
-                                        8:00 AM - 12:00 PM  <br />
-                                        6:00 PM - 8:00 PM
-                                        """, styles['rc-aawp-main-content']),
-                                ]
-                            ],
-                            style=extend_table_style(styles["rc-main-table"], [
-                                ("VALIGN", (0, 0), (-1, -1), "TOP")
-                            ]),
-                            colWidths=(20*mm, 55*mm)
-                        ),
-                    ]
-                ],
-                style=extend_table_style(styles["rc-main-table"], [
-                    ("VALIGN", (0, 0), (-1, -1), "TOP")
-                ]),
-                colWidths=(105*mm, 75*mm)
-            ),
-            Spacer(0, 4.2*mm),
+            Spacer(0, 6.4*mm),            
             Paragraph("""
-                UPON COMPLETION OF FORM, RETURN TO BROOKHAVEN MUNICIPAL COURT FOR [CCH] PROCESSING OF FINGER PRINTS. <br />
-                BROOKHAVEN MUNICIPAL COURT WILL PROVIDE A COPY OF THIS FORM TO THE PROBATION OFFICE.
+                Notes: <br />
+                (1) Cases Open: A count of cases that, at the start of the reporting period, are awaiting disposition <br />
+                (2) Cases Filed: A count of cases that have been filed with the court for the first time during the reporting period. <br />
+                (3) Cases Disposed: A count of cases for which an original entry of judgment has been entered during the reporting period. <br />
                 """, 
-                extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER, fontSize=8)
-            ),
-            Spacer(0, 4.2*mm),
-            Table(
-                [   
-                    [
-                        Paragraph("<b>FULL NAME:</b> JUAN URIEL MELENDEZ HERRERA", styles['rc-aawp-main-content']),
-                        None, None, None
-                    ],
-                    [
-                        Paragraph("<b>GENDER:</b> Male", styles['rc-aawp-main-content']),
-                        Paragraph("<b>RACE:</b> White", styles['rc-aawp-main-content']),
-                        Paragraph("""<b>HEIGHT:</b> 5' 5" """, styles['rc-aawp-main-content']),
-                        Paragraph("<b>WEIGHT:</b> 150", styles['rc-aawp-main-content']),
-                    ],
-                    [
-                        Paragraph("<b>EYE COLOR:</b> Brown", styles['rc-aawp-main-content']),
-                        None,
-                        Paragraph("<b>HAIR COLOR:</b> Brown", styles['rc-aawp-main-content']),
-                        None
-                    ],
-                    [
-                        Paragraph("<b>PLACE OF BIRTH:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                        None,
-                        Paragraph("<b>DATE OF BIRTH:</b> 09/24/1987", styles['rc-aawp-main-content']),
-                        None
-                    ],
-                    [   
-                        Paragraph("<b>RESIDENCE OF PERSON:</b> ", styles['rc-aawp-main-content']),
-                        Paragraph(
-                            """                            
-                            3649 BUFORD HWY NE APT E6 <br />
-                            BROOKHAVEN, GA 30329-1137
-                            """, 
-                            styles['rc-aawp-main-content']
-                        ),
-                        None, None
-                    ],
-                    [
-                        Paragraph("<b>DATE OF ARREST:</b> 10/17/2016", styles['rc-aawp-main-content']),
-                        None,
-                        Paragraph("<b>WARRANT ISSUED:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                        None
-                    ]
-                ],
-                style=extend_table_style(styles["rc-main-table"], [
-                    ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                    ("TOPPADDING", (0, 0), (-1, -1), 1.2 * mm),
-                    ("BOTTOMPADDING", (0, 0), (-1, -1), 1.2 * mm),
-                    ("SPAN", (0, 0), (3, 0)),
-                    ("SPAN", (0, 2), (1, 2)),
-                    ("SPAN", (2, 2), (3, 2)),
-                    ("SPAN", (0, 3), (1, 3)),
-                    ("SPAN", (2, 3), (3, 3)),
-                    ("SPAN", (1, 4), (3, 4)),
-                    ("SPAN", (0, 5), (1, 5)),
-                    ("SPAN", (2, 5), (3, 5)),
-                ]),                
-                colWidths=(45*mm, 45*mm, 45*mm, 45*mm)
+                extend_style(styles['rc-aawp-main-content'], leftIndent=20)
             ),
             Spacer(0, 4.2*mm),
         ]
         violation_table = [
             [
-                Paragraph("<b>Ticket #</b>", styles['rc-aawp-main-content']),
-                Paragraph("<b>Statute</b>", styles['rc-aawp-main-content']),
-                Paragraph("<b>Charge Description</b>", styles['rc-aawp-main-content']),
-                Paragraph("<b>Bond Amount</b>", styles['rc-aawp-main-content']),
-                Paragraph("<b>Disposition</b>", styles['rc-aawp-main-content']),
+                Paragraph("", styles['rc-aawp-main-content']),
+                Paragraph("<b>Cases Open</b>", extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
+                Paragraph("<b>Cases Filed</b>", extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
+                Paragraph("<b>Cases Disposed</b>", extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
+                Paragraph("<b>Bindover</b>", extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
             ]
         ]
         violation_arr = [
             {
-                "Ticket #" : "E31415",
-                "Statute" : "40-6-48 ",
-                "Charge Description" : "FAILURE TO MAINTAIN LANE-1ST",
-                "Bond Amount" : "",
-                "Disposition" : "Pled Guilty"
+                "Ticket" : "SERIOUS TRAFFIC - DUI",
+                "Cases Open" : "55",
+                "Cases Filed" : "13",
+                "Cases Disposed" : "11",
+                "Bindover" : "7"
+            },
+            {
+                "Ticket" : "SERIOUS TRAFFIC - OTHER",
+                "Cases Open" : "312",
+                "Cases Filed" : "82",
+                "Cases Disposed" : "67",
+                "Bindover" : "15"
             },{
-                "Ticket #" : "E31416",
-                "Statute" : "40-5-20",
-                "Charge Description" : "DRIVING WITHOUT A VALID LICENSE-1ST",
-                "Bond Amount" : "",
-                "Disposition" : "Pled Guilty"
+                "Ticket" : "MISDEMEANOR - TRAFFIC",
+                "Cases Open" : "1215",
+                "Cases Filed" : "430",
+                "Cases Disposed" : "379",
+                "Bindover" : "18"
             },{
-                "Ticket #" : "E31417",
-                "Statute" : "10-6-391a'",
-                "Charge Description" : "DUI-ALCOHOL-LESS SAFE-1ST",
-                "Bond Amount" : "",
-                "Disposition" : "Pled Guilty"
+                "Ticket" : "MISDEMEANOR - DRUG",
+                "Cases Open" : "15",
+                "Cases Filed" : "0",
+                "Cases Disposed" : "0",
+                "Bindover" : "0"
             },{
-                "Ticket #" : "E62869",
-                "Statute" : "18-11",
-                "Charge Description" : "PUBLIC INTOXICATION AND PUBLIC CONSUMF",
-                "Bond Amount" : "",
-                "Disposition" : "Pled Guilty"
+                "Ticket" : "MISDEMEANOR - OTHER",
+                "Cases Open" : "511",
+                "Cases Filed" : "83",
+                "Cases Disposed" : "46",
+                "Bindover" : "3"
             },{
-                "Ticket #" : "E62870",
-                "Statute" : "40-6-92",
-                "Charge Description" : "CROSSING ROADWAY ELSEWHERE THAN AT",
-                "Bond Amount" : "",
-                "Disposition" : "Pled Guilty"
+                "Ticket" : "PARKING VIOLATION",
+                "Cases Open" : "304",
+                "Cases Filed" : "7",
+                "Cases Disposed" : "10",
+                "Bindover" : "0"
             },{
-                "Ticket #" : "E62869",
-                "Statute" : "9-7",
-                "Charge Description" : "FAILURE TO APPEAR",
-                "Bond Amount" : "",
-                "Disposition" : "Pled Guilty"
+                "Ticket" : "ORDINANCE",
+                "Cases Open" : "1050",
+                "Cases Filed" : "26",
+                "Cases Disposed" : "20",
+                "Bindover" : "1"
+            },{
+                "Ticket" : "TOTAL",
+                "Cases Open" : "3642",
+                "Cases Filed" : "641",
+                "Cases Disposed" : "533",
+                "Bindover" : "44"
             }
         ]
         for idx, violation in enumerate(violation_arr):
             violation_table.append(
                 [
-                    Paragraph(violation["Ticket #"], styles['rc-aawp-main-content']),
-                    Paragraph(violation["Statute"], styles['rc-aawp-main-content']),
-                    Paragraph(violation["Charge Description"], styles['rc-aawp-main-content']),
-                    Paragraph(violation["Bond Amount"], styles['rc-aawp-main-content']),
-                    Paragraph(violation["Disposition"], styles['rc-aawp-main-content']),
+                    Paragraph(violation["Ticket"], styles['rc-aawp-main-content']),
+                    Paragraph(violation["Cases Open"], extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
+                    Paragraph(violation["Cases Filed"], extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
+                    Paragraph(violation["Cases Disposed"], extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
+                    Paragraph(violation["Bindover"], extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
                 ]
             )  
         elems +=[
@@ -310,51 +221,13 @@ class MCCRReport:
                 style=extend_table_style(styles["rc-main-table"], [
                     ("VALIGN", (0, 0), (-1, -1), "TOP"),
                     ("GRID", (0, 0), (-1, -1),  .1, "black"),
-                    ("TOPPADDING", (0, 0), (-1, -1), 1.8 * mm),
-                    ("BOTTOMPADDING", (0, 0), (-1, -1), 1.8 * mm),
-                    ("LEFTPADDING", (0, 0), (-1, -1), 1.8 * mm),
-                    ("RIGHTPADDING", (0, 0), (-1, -1), 1.8 * mm),
+                    ("TOPPADDING", (0, 0), (-1, -1), 1.4 * mm),
+                    ("BOTTOMPADDING", (0, 0), (-1, -1), 1.4 * mm),
+                    ("LEFTPADDING", (0, 0), (-1, -1), 1.4 * mm),
+                    ("RIGHTPADDING", (0, 0), (-1, -1), 1.4 * mm),
                 ]),
-                colWidths=(25*mm, 25*mm, 85*mm, 30*mm, 30*mm)
-            ),
-            Spacer(0, 5*mm),
-            Table(
-                [   
-                    [
-                        Paragraph("<b>PROBATION:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                        Paragraph("<b>END DATE:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                    ],
-                    [
-                        Paragraph("<b>WITH:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                        Paragraph("<b>OFFICIAL TAKING PRINTS:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                    ],
-                    [
-                        Paragraph("<b>OTN:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content']),
-                        Paragraph("<b>DATE:</b> {}".format(TEST_DATA), styles['rc-aawp-main-content'])
-                    ]
-                ],
-                style=extend_table_style(styles["rc-main-table"], [
-                    ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                    ("TOPPADDING", (0, 0), (-1, -1), 1.2 * mm),
-                    ("BOTTOMPADDING", (0, 0), (-1, -1), 1.2 * mm),
-                ]),
-                colWidths=(90*mm, 90*mm)
-            ),
-            Spacer(0, 4.2*mm),
-            Paragraph("<b>***Finger Prints WILL NOT be taken without valid Photo ID***</b>", extend_style(styles['rc-aawp-main-content'], alignment=TA_CENTER)),
-            Spacer(0, 4.2*mm),
-            Table(
-                [   
-                    [
-                        Paragraph("Clerk Name", extend_style(styles['rc-aawp-main-content'], fontSize=12)),
-                        Paragraph("Clerk Signature", extend_style(styles['rc-aawp-main-content'], fontSize=12)),
-                    ],
-                ],
-                style=extend_table_style(styles["rc-main-table"], [
-                    ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                ]),
-                colWidths=(140*mm, 50*mm)
-            ),
+                colWidths=(70*mm, 30*mm, 30*mm, 30*mm, 30*mm)
+            )
         ]
         
         return elems
