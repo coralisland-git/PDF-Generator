4047693c1361954dd12c441b8fb2b32c5ae01678
AmadeuscyfCao <amadeuscyf@gmail.com>
2020-04-06 11:27:47 -0500

    replace underline with checkbox

--- certificate_of_service/generate_certificate_of_service.py
+++ certificate_of_service/generate_certificate_of_service.py
@@ -100,9 +100,9 @@ def generate_certificate_of_service():
                     Paragraph(
                         """
                         <br/>
-                        Case Number: E31417
+                        <b>Case Number: E31417</b>
                         """,
-                        styles["rc-aawp-main-header"],
+                        styles["rc-doc-sub-header"],
                     ),
                 ],
                 [None],
@@ -124,40 +124,93 @@ def generate_certificate_of_service():
         ),
         Spacer(0, 6 * mm),
         Paragraph(
-            "This is to certify that I have this day served by the attorney appointed by the court in the above-styled "
-            "case with a true and correct copy of the document(s) checked below: __ by depositing same in the U.S. "
-            "Mail, properly addressed and with sufficient postage affixed to insure delivery"
-            " or __ by email provided by the appointed attorney.",
-            style=styles["rc-aawp-main-content"],
-        ),
-        Spacer(0, 10 * mm),
-        Paragraph("__ a. Copy of Citation(s)", style=styles["rc-aawp-main-content"]),
-        Spacer(0, 6 * mm),
-        Paragraph(
-            "__ b. List of Witnesses - [AS CONTAINED IN ATTACHED (PORTION OF) INCIDENT AND/OR ACCIDENT REPORT "
-            "& Scientific Reports)",
+            "This is to certify that I have this day served by the attorney appointed by the court in the "
+            "above-styled case with a true and correct copy of the document(s) checked below:",
             style=styles["rc-aawp-main-content"],
         ),
         Spacer(0, 6 * mm),
-        Paragraph(
-            "__ c. Defendant's Oral Statements - [AS CONTAINED IN ATTACHED (PORTION OF) INCIDENT AND/OR "
-            "ACCIDENT REPORT",
-            style=styles["rc-aawp-main-content"],
+        Table(
+            [
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "by depositing same in the U.S.Mail, properly addressed and with sufficient postage affixed to "
+                        "insure delivery or ", style=styles["rc-aawp-main-content"]),
+                ],
+                [None],
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "by email provided by the appointed attorney.", style=styles["rc-aawp-main-content"]),
+                ]
+            ],
+            style=extend_table_style(
+                styles["rc-main-table"], [("VALIGN", (0, 0), (-1, -1), "TOP")]
+            ),
+            colWidths=(0 * mm, 6 * mm, 194 * mm)
         ),
-        Spacer(0, 6 * mm),
-        Paragraph(
-            "__ d. Defendant's Written Statements - [ATTACHED][N/A]",
-            style=styles["rc-aawp-main-content"],
-        ),
-        Spacer(0, 6 * mm),
-        Paragraph(
-            "__ e. Written Scientific Reports - Intox or Division of Forensic Science Report",
-            style=styles["rc-aawp-main-content"],
-        ),
-        Spacer(0, 6 * mm),
-        Paragraph(
-            "__ f. Form for request of video if applicable",
-            style=styles["rc-aawp-main-content"],
+        Spacer(0, 15 * mm),
+        Table(
+            [
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph("a. Copy of Citation(s)", style=styles["rc-aawp-main-content"]),
+                ],
+                [None],
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "b. List of Witnesses - [AS CONTAINED IN ATTACHED (PORTION OF) INCIDENT AND/OR ACCIDENT REPORT "
+                        "& Scientific Reports)",
+                        style=styles["rc-aawp-main-content"],
+                    ),
+                ],
+                [None],
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "c. Defendant's Oral Statements - [AS CONTAINED IN ATTACHED (PORTION OF) INCIDENT AND/OR "
+                        "ACCIDENT REPORT",
+                        style=styles["rc-aawp-main-content"],
+                    ),
+                ],
+                [None],
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "d. Defendant's Written Statements - [ATTACHED][N/A]",
+                        style=styles["rc-aawp-main-content"],
+                    ),
+                ],
+                [None],
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "e. Written Scientific Reports - Intox or Division of Forensic Science Report",
+                        style=styles["rc-aawp-main-content"],
+                    ),
+                ],
+                [None],
+                [
+                    XBox(10, False),
+                    None,
+                    Paragraph(
+                        "f. Form for request of video if applicable",
+                        style=styles["rc-aawp-main-content"],
+                    ),
+                ],
+            ],
+            style=extend_table_style(
+                styles["rc-main-table"], [("VALIGN", (0, 0), (-1, -1), "TOP")]
+            ),
+            colWidths=(0 * mm, 6 * mm, 194 * mm),
         ),
         Spacer(0, 15 * mm),
         Paragraph("The day of 07/16/2019", style=styles["rc-aawp-main-content"]),
@@ -174,3 +227,19 @@ def generate_certificate_of_service():
 
     buff.seek(0)
     return buff
+
+class XBox(Flowable):
+    def __init__(self, size, checked=None):
+        Flowable.__init__(self)
+        self.width = size
+        self.height = size
+        self.size = size
+        self.checked = checked
+
+    def draw(self):
+        self.canv.saveState()
+        self.canv.setLineWidth(0.11 * self.size)
+        self.canv.rect(0, 0, self.width, self.height)
+        if self.checked is True:
+            self.check()
+        self.canv.restoreState()
