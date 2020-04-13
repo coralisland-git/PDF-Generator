4f86aa7ab1539b240ca4ce86f2e58b59e13448cb
AmadeuscyfCao <amadeuscyf@gmail.com>
2020-04-13 12:11:25 -0500

    initialize code

---
+++ local_victim_assistance_fund_remittance_report/sample_data.py
@@ -0,0 +1,9 @@
+local_victim_remmitance_report_data = {
+    "county/city": "Rockdale County",
+    "court_name": "Magistrate Court Rockdale County",
+    "ori_number": "GA 1220335",
+    "date_range_from": "11/01/2014",  # %m/%d/%Y
+    "date_range_to": "11/30/2014",  # %m/%d/%Y
+    "total_amount": 547.50,
+    "order_date": "12/31/2014",  # %m/%d/%Y
+}
