a0a51b27ce838cee79da2046248cb10844a71c0f
AmadeuscyfCao <amadeuscyf@gmail.com>
2020-04-20 15:30:10 -0500

    finish

--- monthly_disbursement_report/document_specific_styles.py
+++ monthly_disbursement_report/document_specific_styles.py
@@ -1,1 +1,28 @@
 from common.reportlab_styles import *
+
+styles["rc-mdr-main-content"] = ParagraphStyle(
+    "rc-mdr-main-content",
+    parent=styles["rc-aawp-main"],
+    fontName="Arial",
+    fontSize=9.5,
+    leading=15,
+)
+
+styles["rc-mdr-header"] = ParagraphStyle(
+    "rc-mdr-header",
+    parent=styles["rc-header"],
+    fontName="Times-Roman",
+    fontSize=15,
+    leading=15,
+)
+
+styles["footer"] = ParagraphStyle(
+    "footer",
+    parent=styles["detail-slimfit"],
+)
+
+styles["signature"] = ParagraphStyle(
+    "signature",
+    parent=styles["body"],
+    fontSize=9.5,
+)
