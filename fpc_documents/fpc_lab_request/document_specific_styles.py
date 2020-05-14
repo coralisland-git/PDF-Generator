6049c2e66108e343066075db687255d10dfb9bd9
coralisland-git <coralisland8327@gmail.com>
2020-05-15 01:18:22 +0200

    add generate_fpc_documents for lab_requuest, property_control_report, receipt

--- fpc_documents/fpc_lab_request/document_specific_styles.py
+++ fpc_documents/fpc_lab_request/document_specific_styles.py
@@ -1,3 +1,49 @@
+import os
 from common.reportlab_styles import *
+from reportlab.lib.enums import TA_JUSTIFY, TA_LEFT, TA_CENTER, TA_RIGHT
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.pdfmetrics import registerFontFamily, stringWidth
 
 # document specific styles go here
+
+pdfmetrics.registerFont(
+    TTFont("Arial-Italic", os.path.join(os.getcwd(), "fonts", "arialit.ttf"))
+)
+
+styles["rc-doc-header"] = ParagraphStyle(
+    "rc-doc-header",
+    parent=styles["rc-doc-header"],
+    fontName="Arial-Bold",    
+    leading=18,
+    fontSize=13
+)
+
+styles["rc-header"] = ParagraphStyle(
+    "rc-header",
+    parent=styles["rc-header"],
+    fontName="Arial",
+    spaceBefore=22,
+    fontSize=13,
+    spaceAfter=20
+)
+
+styles["rc-aawp-main-content"] = ParagraphStyle(
+    "rc-aawp-main-content",
+    parent=styles["rc-aawp-main"],
+    leading=14,
+    fontSize=10,
+    fontName="Arial"
+)
+
+styles["rc-aawp-main-date"] = ParagraphStyle(
+    "rc-aawp-main-date",
+    parent=styles["rc-aawp-main-content"],    
+    fontName="Arial-Italic"
+)
+
+styles["rc-aawp-main-content-tb"] = ParagraphStyle(
+    "rc-aawp-main-content-tb",
+    parent=styles["rc-aawp-main-content"],
+    alignment=TA_CENTER
+)
